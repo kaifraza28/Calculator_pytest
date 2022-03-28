6f4ab165a5f35e2806d63ed2c1eb4caa8991959f
kaifraza28 <itzkaifraza@gmail.com>
2022-03-28 15:01:24 +0530

    Python pytesting

---
+++ test_Calculator_New.py
@@ -0,0 +1,21 @@
+import Calculator_New
+def test_add():
+    x = 10
+    y = 20
+    result = Calculator_New.add(x,y)
+    assert x+y==result
+def test_sub():
+    x = 10
+    y = 20
+    result = Calculator_New.sub(x,y)
+    assert x-y==result
+def test_mult():
+    x = 10
+    y = 20
+    result = Calculator_New.mult(x,y)
+    assert x*y==result
+def test_div():
+    x = 10
+    y = 20
+    result = Calculator_New.div(x,y)
+    assert x/y==result
